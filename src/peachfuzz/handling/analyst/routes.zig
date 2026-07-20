53fb65ed8918e1606fed0e663bf101ca54cf41dd
gcca <git@gcca.dev>
2026-07-20 10:01:47 -0500

    20260720100147

--- src/peachfuzz/handling/analyst/routes.zig
+++ src/peachfuzz/handling/analyst/routes.zig
@@ -1,10 +1,7 @@
 const httplib = @import("httplib");
 
-const analystGet = @import("handlers/analyst-get.zig").analystGet;
-const pageGet = @import("handlers/page-get.zig").pageGet;
-
 pub fn initRoutes(server: httplib.Server) void {
     _ = server
-        .Get("/peachfuzz/analyst", analystGet)
-        .Get("/peachfuzz/analyst/pages/:name", pageGet);
+        .Get("/peachfuzz/analyst", @import("handlers/analyst-get.zig").analystGet)
+        .Get("/peachfuzz/analyst/pages/:name", @import("handlers/page-get.zig").pageGet);
 }
--- src/peachfuzz/handling/home/routes.zig
+++ src/peachfuzz/handling/home/routes.zig
@@ -1,7 +1,5 @@
 const httplib = @import("httplib");
 
-const homeGet = @import("handlers/home-get.zig").homeGet;
-
 pub fn initRoutes(server: httplib.Server) void {
-    _ = server.Get("/peachfuzz/home", homeGet);
+    _ = server.Get("/peachfuzz/home", @import("handlers/home-get.zig").homeGet);
 }
